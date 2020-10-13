50089b9fdc4c453f51899ea1d6ec758ee238b522
Prateek Patel <prateekpatel@Prateeks-MacBook-Pro.local>
2020-10-13 12:21:49 +0530

    added build folder

---
+++ build/precache-manifest.79f243964da5ecaa1c0df473b0484789.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8caa09d78fa7d07006665dd16b488b94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57345031d61a7dcfd2b6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cfd80c557bab03211969",
+    "url": "/static/js/2.38458f08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38458f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57345031d61a7dcfd2b6",
+    "url": "/static/js/main.885424f7.chunk.js"
+  },
+  {
+    "revision": "2f8ee3516eb811d0b8a7",
+    "url": "/static/js/runtime-main.a9d4158b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
